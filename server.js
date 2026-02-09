10a6265cb5d011d4348afd5d07b1b625ac7b47f7
Yugal Raj Vimal <yugalrajvimal@Yugals-MacBook-Air.local>
2026-02-09 21:38:36 +0530

    --

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8811;
 
 app.use("/Uploads/Therapist", express.static("Uploads/Therapist"));
 
